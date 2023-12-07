f746ff422a7e3f8369943ec7a9b145332936cd0a
AlynaaG <alynaa.giurca@yahoo.com>
2023-12-07 18:20:02 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import { Moon, Sun } from "lucide-react";
 
-function App() {
+export default function App() {
+  const ref = useRef();
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_BASE_URL}${ref.current}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+    );
+    const result = await response.json();
+    setData(result);
+  };
+
+  const handleClick = async () => {
+    console.log(
+      `${process.env.REACT_APP_BASE_URL}${ref.current}&appid=${process.env.REACT_APP_API_KEY}`
+    );
+    getData();
+  };
+
+  const convertTime = (unix_timestamp) => {
+    var date = new Date(unix_timestamp * 1000);
+
+    var hours = date.getHours();
+
+    var minutes = "0" + date.getMinutes();
+
+    var seconds = "0" + date.getSeconds();
+
+    var formattedTime = hours + ":" + minutes.substr(-2);
+
+    return formattedTime;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <div className="input">
+          <input
+            type="search"
+            placeholder="Type city...."
+            onChange={(e) => (ref.current = e.target.value)}
+          />
+        </div>
+
+        <button className="button" onClick={handleClick}>
+          Search
+        </button>
+      </div>
+
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <h2> {data?.name}</h2>
+          </div>
+
+          <div className="temp">
+            <h1> Temp: {data?.main?.temp} &deg;C </h1>
+          </div>
+
+          <div className="description">
+            <img
+              src={`https://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`}
+              alt={data?.weather[0].description}
+            />
+            <spam>{data?.weather[0].description}</spam>
+          </div>
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div className="feels">
+          <p>Feels Like </p>
+          <p className="bold">{data?.main?.feels_like}</p>
+        </div>
+        <div className="humidity">
+          <p>Humidity</p>
+          <p className="bold">{data?.main?.humidity}</p>
+        </div>
+        <div className="wind">
+          <p>Wind Speed</p>
+          <p className="bold">{data?.wind?.speed}</p>
+        </div>
+      </div>
+      <div className="time">
+        <div className="Sunrise">
+          <span>
+            <Moon />
+            Sunrise:{" "}
+          </span>
+
+          <span>{convertTime(data?.sys?.sunrise)}</span>
+        </div>
+        <div className="Sunset">
+          <span>
+            <Sun />
+            Sunset:{" "}
+          </span>
+          <span>{convertTime(data?.sys?.sunset)}</span>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    backgroundImage: {
+      sky: "linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(9,9,121,0.9114583333333334) 35%, rgba(0,212,255,1) 100%);",
+    },
+  },
+  plugins: [],
+};
