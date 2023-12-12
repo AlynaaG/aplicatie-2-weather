9a8ddf8888292aa1a96e170fe248abebb927bc76
AlynaaG <alynaa.giurca@yahoo.com>
2023-12-12 13:40:38 +0200

    final changes

--- src/App.js
+++ src/App.js
@@ -6,31 +6,27 @@ export default function App() {
   const [data, setData] = useState();
 
   const getData = async () => {
-    const response = await fetch(
-      `${process.env.REACT_APP_BASE_URL}${ref.current}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
-    );
-    const result = await response.json();
-    setData(result);
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_BASE_URL}${ref.current}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+      );
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      // Handle errors here
+    }
   };
 
   const handleClick = async () => {
-    console.log(
-      `${process.env.REACT_APP_BASE_URL}${ref.current}&appid=${process.env.REACT_APP_API_KEY}`
-    );
     getData();
   };
 
   const convertTime = (unix_timestamp) => {
     var date = new Date(unix_timestamp * 1000);
-
     var hours = date.getHours();
-
     var minutes = "0" + date.getMinutes();
-
-    var seconds = "0" + date.getSeconds();
-
     var formattedTime = hours + ":" + minutes.substr(-2);
-
     return formattedTime;
   };
 
@@ -50,57 +46,64 @@ export default function App() {
         </button>
       </div>
 
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <h2> {data?.name}</h2>
+      {data ? (
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <h2> {data?.name}</h2>
+            </div>
+
+            <div className="temp">
+              <h1> Temp: {data?.main?.temp} &deg;C </h1>
+            </div>
+
+            <div className="description">
+              <img
+                src={`https://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`}
+                alt={data?.weather[0].description}
+              />
+              <span>{data?.weather[0].description}</span>
+            </div>
           </div>
+        </div>
+      ) : (
+        ""
+      )}
 
-          <div className="temp">
-            <h1> Temp: {data?.main?.temp} &deg;C </h1>
+      {data && (
+        <div>
+          <div className="bottom">
+            <div className="feels">
+              <p>Feels Like </p>
+              <p className="bold">{data?.main?.feels_like}</p>
+            </div>
+            <div className="humidity">
+              <p>Humidity</p>
+              <p className="bold">{data?.main?.humidity}</p>
+            </div>
+            <div className="wind">
+              <p>Wind Speed</p>
+              <p className="bold">{data?.wind?.speed}</p>
+            </div>
           </div>
-
-          <div className="description">
-            <img
-              src={`https://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`}
-              alt={data?.weather[0].description}
-            />
-            <spam>{data?.weather[0].description}</spam>
+          <div className="time">
+            <div className="Sunrise">
+              <span>
+                <Moon />
+                Sunrise:{" "}
+              </span>
+              <span>{convertTime(data?.sys?.sunrise)}</span>
+            </div>
+            <div className="Sunset">
+              <span>
+                <Sun />
+                Sunset:{" "}
+              </span>
+              <span>{convertTime(data?.sys?.sunset)}</span>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className="bottom">
-        <div className="feels">
-          <p>Feels Like </p>
-          <p className="bold">{data?.main?.feels_like}</p>
-        </div>
-        <div className="humidity">
-          <p>Humidity</p>
-          <p className="bold">{data?.main?.humidity}</p>
-        </div>
-        <div className="wind">
-          <p>Wind Speed</p>
-          <p className="bold">{data?.wind?.speed}</p>
-        </div>
-      </div>
-      <div className="time">
-        <div className="Sunrise">
-          <span>
-            <Moon />
-            Sunrise:{" "}
-          </span>
-
-          <span>{convertTime(data?.sys?.sunrise)}</span>
-        </div>
-        <div className="Sunset">
-          <span>
-            <Sun />
-            Sunset:{" "}
-          </span>
-          <span>{convertTime(data?.sys?.sunset)}</span>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
